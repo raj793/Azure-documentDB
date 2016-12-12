db9c728640eebfa3ee93d136c561f40ba6dfa876
Shriraj <shri.93.raj@gmail.com>
2016-12-12 16:14:07 +0530

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private const string EndpointUrl = "localhost:8081";
+        private const string key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                CreateDocumentClient().Wait();
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static async Task CreateDocumentClient()
+        {
+            // Create a new instance of the DocumentClient
+            var client = new DocumentClient(new Uri(EndpointUrl), key);
+        }
+    }
+    
+}
